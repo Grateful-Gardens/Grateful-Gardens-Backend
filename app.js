09d1524f4c8d6a91047c9f0229b31307ae784e09
Zohaib <zohaibmanzoor1700@gmail.com>
2022-05-11 22:28:28 -0700

    minor fix on formatting

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors'); 
 const app = express();
-const PORT = 9001; app.use(express.json());
+const PORT = 9001; 
+app.use(express.json());
 app.use(cors())
 
 app.listen(PORT, () => {

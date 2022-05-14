3b618e82b1de17d83b74b3d8797fa347bdbdc339
Jah821 <Jahsanni@gmail.com>
2022-05-14 10:37:07 -0400

    started user and post routes have to finish likes comments friendships and bookmarks routers.. I am still having toruble acceessing the routes through local host all of the sql queries work within the databse though. One of u can try to see if u can access the routes through ur local host. Gonna Ask Erwin to help me fix the problem.

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
+import router from ('./routes/postRouter')
+import router from('./routes/userRouter')
+
 const express = require('express');
+
 const cors = require('cors'); 
+
 const app = express();
+
 const PORT = 9001; 
+
+
+
 app.use(express.json());
 app.use(cors())
+
+router.use(postRouter);
+router.use(userRouter);
+
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${ PORT }`)

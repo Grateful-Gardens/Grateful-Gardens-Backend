0f2f2b0af7860b18b892ee16cee161aa5b8d372d
Zohaib <zohaibmanzoor1700@gmail.com>
2022-05-19 19:53:06 -0400

    comments CRU

--- model/postsModel.js
+++ model/postsModel.js
@@ -1,66 +1,75 @@
-const pool = require('../db')
+const pool = require("../db");
 
 class PostsModel {
-    static async getAllPosts() {
-        const sql = `SELECT post_id, hashtag,image, description, time_posted, users.user_id, username FROM posts 
-        JOIN users ON posts.user_id = users.user_id order by time_posted desc`
-        // SELECT posts.post_id, hashtag,image, description, time_posted, users.user_id, username, my_like_count
-        // FROM posts 
-        // JOIN users 
-        // ON posts.user_id = users.user_id
-        // JOIN (SELECT post_id, COUNT(post_id) AS my_like_count FROM likes GROUP BY post_id)
-        // AS like_counter
-        // ON posts.post_id = like_counter.post_id
-        const dbResult = await pool.query(sql);
-        return dbResult.rows
-    }
+  static async getAllPosts() {
+    const sql = `SELECT post_id, hashtag,image, description, time_posted, users.user_id, username FROM posts 
+        JOIN users ON posts.user_id = users.user_id order by time_posted desc`;
+    // SELECT posts.post_id, hashtag,image, description, time_posted, users.user_id, username, my_like_count
+    // FROM posts
+    // JOIN users
+    // ON posts.user_id = users.user_id
+    // JOIN (SELECT post_id, COUNT(post_id) AS my_like_count FROM likes GROUP BY post_id)
+    // AS like_counter
+    // ON posts.post_id = like_counter.post_id
+    const dbResult = await pool.query(sql);
+    return dbResult.rows;
+  }
 
-    static async createPost(data) {
-        const { hashtag, image, description, user_id } = data
-        const sql = `INSERT INTO posts (hashtag, image, description, user_id) VALUES ($1, $2, $3, $4) RETURNING *`;
-        const dbResult = await pool.query(sql, [hashtag, image, description, user_id])
-        return dbResult.rows
-    }
+  static async createPost(data) {
+    const { hashtag, image, description, user_id } = data;
+    const sql = `INSERT INTO posts (hashtag, image, description, user_id) VALUES ($1, $2, $3, $4) RETURNING *`;
+    const dbResult = await pool.query(sql, [
+      hashtag,
+      image,
+      description,
+      user_id,
+    ]);
+    return dbResult.rows;
+  }
 
-    static async deleteAPost(post_id) {
-        if (!post_id) throw new Error(`POST WITH ID:${post_id} DOES NOT EXIST`)
-        const sql = `DELETE FROM posts WHERE post_id = ($1)`;
-        const dbResult = await pool.query(sql, [post_id])
-        return dbResult.rows[0]
-    }
+  static async deleteAPost(post_id) {
+    if (!post_id) throw new Error(`POST WITH ID:${post_id} DOES NOT EXIST`);
+    const sql = `DELETE FROM posts WHERE post_id = ($1)`;
+    const dbResult = await pool.query(sql, [post_id]);
+    return dbResult.rows[0];
+  }
 
-    // Change to getBookmarkedPosts()
-    static async getAllOfAUsersPost(post_id) {
-        if (!post_id) throw new Error(`POSTS WITH ID:${post_id} DO NOT EXIST`)
-        const sql = `SELECT * FROM posts WHERE post_id = ($1)`;
-        const dbResult = await pool.query(sql, [post_id])
-        return dbResult.rows[0]
-    }
+  static async getAllOfAUsersPost(post_id) {
+    if (!post_id) throw new Error(`POSTS WITH ID:${post_id} DO NOT EXIST`);
+    const sql = `SELECT * FROM posts WHERE post_id = ($1)`;
+    const dbResult = await pool.query(sql, [post_id]);
+    return dbResult.rows[0];
+  }
 
-    static async findSpecificPost(post_id) {
-        if (!post_id) throw new Error(`POST WITH ID:${post_id} DOES NOT EXIST`)
-        const sql = `SELECT * FROM posts WHERE post_id = ($1)`;
-        const dbResult = await pool.query(sql, [post_id])
-        return dbResult.rows[0]
-    }
-    // static async findPostByHashTag(hashtag) {
-    //     const sql = `SELECT * FROM posts WHERE hashtag = ($1)`;
-    //     const dbResult = await pool.query(sql, [hashtag])
-    //     return dbResult.rows
-    // }
+  static async findSpecificPost(post_id) {
+    if (!post_id) throw new Error(`POST WITH ID:${post_id} DOES NOT EXIST`);
+    const sql = `SELECT * FROM posts WHERE post_id = ($1)`;
+    const dbResult = await pool.query(sql, [post_id]);
+    return dbResult.rows[0];
+  }
 
-    // static async updatePost(post_id, description) {
-    //     const sql = `UPDATE posts SET description = ($2) WHERE post_id = ($1)`;
-    //     const dbResult = await pool.query(sql, [post_id, description])
-    //     return dbResult.rows
-    // }
+  static async getLikes(post_id) {
+    if (!post_id) throw new Error(`POST WITH ID:${post_id} DOES NOT EXIST`);
+    const sql = `SELECT COUNT($1) from likes`;
+    const dbResult = await pool.query(sql, [post_id]);
+    return dbResult.rows[0];
+  }
 
-    static async getLikes(post_id) {
-        if (!post_id) throw new Error(`POST WITH ID:${post_id} DOES NOT EXIST`)
-        const sql = `SELECT COUNT(${post_id}) from likes`;
-        const dbResult = await pool.query(sql, [post_id])
-        return dbResult.rows[0]
-    }
+  static async getComments(post_id) {
+    if (!post_id) throw new Error(`POST WITH ID:${post_id} DOES NOT EXIST`);
+    const sql = `SELECT comments.*, users.username FROM comments 
+        JOIN users ON comments.user_id = users.user_id
+        WHERE post_id = $1`;
+    const dbResult = await pool.query(sql, [post_id]);
+    return dbResult.rows;
+  }
+
+  static async postComment(data) {
+    const { comment_body, user_id, post_id } = data;
+    const sql = `INSERT INTO comments (comment_body, user_id, post_id) VALUES ($1, $2, $3) RETURNING *`;
+    const dbResult = await pool.query(sql, [comment_body, user_id, post_id]);
+    return dbResult.rows;
+  }
 }
 
-module.exports = PostsModel
+module.exports = PostsModel;
--- routes/postRouter.js
+++ routes/postRouter.js
@@ -1,21 +1,32 @@
-const router = require('express').Router();;
-const { fetchPosts, createPost, getAllOfUsersPost, hashTagPost, deleteAPost, updatePosts, getLikesForAPost} = require('../controller/postController')
+const router = require("express").Router();
+const {
+  fetchPosts,
+  createPost,
+  getAllOfUsersPost,
+  hashTagPost,
+  deleteAPost,
+  updatePosts,
+  getLikesForAPost,
+  getComments, 
+  postComment
+} = require("../controller/postController");
 
-router.get('/posts', fetchPosts);
+router.get("/posts", fetchPosts);
 
-router.get('/posts/:id', getAllOfUsersPost);
+router.get("/posts/:id", getAllOfUsersPost);
 
-router.get('/posts/:id', hashTagPost);
+router.get("/posts/:id", hashTagPost);
 
-router.post('/posts', createPost);
+router.get("/posts/:id/comments", getComments);
 
-router.patch('/posts/:id', updatePosts);
+router.post("/posts/:id/comments", postComment);
 
-router.delete('/posts/:id', deleteAPost);
+router.post("/posts", createPost);
 
-router.get('/likes/:id', getLikesForAPost)
+router.patch("/posts/:id", updatePosts);
 
+router.delete("/posts/:id", deleteAPost);
 
+router.get("/posts/:id/likes", getLikesForAPost);
 
-
-module.exports = router
+module.exports = router;
